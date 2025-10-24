ae634bfdc3e7ca01827a107fb8bede9602aecd6b
Nicolas Alves <nicke.rodrigues1125@gmail.com>
2025-10-24 21:49:20 +0200

    :sparkles: feat(Gemini): implement generate text

--- api.flux/src/main/java/com/api/flux/controller/GeminiController.java
+++ api.flux/src/main/java/com/api/flux/controller/GeminiController.java
@@ -1,9 +1,23 @@
 package com.api.flux.controller;
 
+import com.api.flux.service.GeminiService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/api/gemini")
 public class GeminiController {
+    private final GeminiService geminiService;
+
+    public GeminiController(GeminiService geminiService) {
+        this.geminiService = geminiService;
+    }
+
+    @PostMapping
+    public ResponseEntity generateText(@RequestBody String prompt) {
+        return geminiService.generatePrompt(prompt);
+    }
 }
--- api.flux/src/main/java/com/api/flux/service/GeminiService.java
+++ api.flux/src/main/java/com/api/flux/service/GeminiService.java
@@ -1,7 +1,37 @@
 package com.api.flux.service;
 
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GeminiService {
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public ResponseEntity generatePrompt(String prompt) {
+        try {
+            Client client = new Client();
+            Client.builder().apiKey(geminiApiKey);
+
+            GenerateContentResponse response =
+                    client.models.generateContent(
+                            "gemini-2.5-flash",
+                            "Instrução de Sistema: Por favor, forneça todas as respostas como texto simples (plain text). Não utilize nenhuma formatação Markdown. Especificamente, não inclua asteriscos (*), barras invertidas (\\) ou outros caracteres de formatação como também para pular linha. A resposta deve ser um texto limpo, sem marcadores ou ênfase, pronto para exibição direta." + prompt,
+                            null
+                    );
+
+            if (response.text().isEmpty()) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
