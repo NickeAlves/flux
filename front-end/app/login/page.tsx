1c8bd53da1e0b21741c88267d1c84b7d656de4d1
Nicolas Alves <nicke.rodrigues1125@gmail.com>
2025-11-07 10:26:53 +0100

    :hammer: fix(Login): fix page name

--- front-end/app/login/page.tsx
+++ front-end/app/login/page.tsx
@@ -74,7 +74,7 @@ const Login: NextPage = () => {
   return (
     <>
       <head>
-        <title>Sign in | Flux</title>
+        <title>Login | Flux</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
